563ecd2b029e1066462378db3a064e98f0d71ea0
Shlok D. Mange <shlok_mange@protonmail.com>
2021-06-30 19:55:26 +0400

    Fixed TS errors

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useAuth } from '../context/AuthContext'
-import { BrowserRouter, Switch, Link} from 'react-router-dom'
 
 const LoginScreen = () => {
     const { googleSignIn } = useAuth()
